0ee9d9adb4254d5262bf2103a56bbcf9cf8d209c
SneakBug8 <sneakbug8@gmail.com>
2018-04-04 22:55:15 +0300

    RecordsDrawer

---
+++ Assets/scripts/UI/RecordsDrawer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordsDrawer : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable () {
+		var record = PlayerPrefs.GetFloat("RecordFloatingTime");
+		var recordtext = (LevelController.Global.FloatingTime > record) ? " New record!" : "";
+		GetComponent<Text>().text = string.Format(
+			"Your score - {0}.{1}",
+			LevelController.Global.FloatingTime,
+			recordtext
+			);
+	}
+}
