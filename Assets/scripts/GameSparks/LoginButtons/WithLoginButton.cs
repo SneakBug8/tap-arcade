6c96b86b30ae17f1116302a9fe21a3f604d3851b
SneakBug8 <sneakbug8@gmail.com>
2018-04-11 12:44:55 +0300

    Working auth with login

---
+++ Assets/scripts/GameSparks/LoginButtons/WithLoginButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WithLoginButton : MonoBehaviour {
+    public Button Button;
+    public InputField Input;
+    public Text PlaceHolder;
+    private void Start() {
+        Button.onClick.AddListener(() => {
+            if (Input.text != "") {
+                LoginManager.Auth(Input.text, RegistrationError);
+                Input.text = "";
+            }
+            else {
+                PlaceHolder.text = "Please, enter valid login";
+                Input.text = "";
+            }
+        });
+    }
+ 
+    void RegistrationError(string login) {
+        PlaceHolder.text = "This login is already taken";
+        Input.text = "";
+    }
+}
